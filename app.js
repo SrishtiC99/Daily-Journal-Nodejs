7b04aea88669b04b7f4b2aa053e1a5498b6bb9e3
SrishtiC99 <iamsri9631@gmail.com>
2023-01-03 15:02:43 +0530

    Mongoose added for rendering post items

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 // Load the full build.
 var _ = require('lodash');
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const postList = [];
 
 const app = express();
 
@@ -18,11 +18,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+main().catch(err=> console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/dailyBlogDB')
+}
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = new mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    postList: postList
-  });
+  Post.find({}, function(err, posts){
+    if(!err){
+      res.render("home", {
+        startingContent: homeStartingContent,
+        postList: posts
+      });
+    }
+  })
 })
 
 
@@ -43,24 +60,27 @@ app.get("/compose", function(req, res){
 })
 
 app.post("/compose", function(req, res){
-  let post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  }
-  postList.push(post);
-  res.redirect("/");
+  let postTitle = req.body.postTitle;
+  let postBody = req.body.postBody;
+
+  let post = new Post({
+    title: postTitle,
+    content: postBody
+  })
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 })
 
-app.get("/posts/:postTitle", function(req, res){
-  postList.forEach(post=> {
-    if(_.lowerCase(post.title) === _.lowerCase(req.params.postTitle)){
+app.get("/posts/:postId", function(req, res){
+  Post.findById(req.params.postId, function(err, post){
+    if(!err){
       res.render("post", {
         title: post.title,
         content: post.content
       })
-    }
-    else{
-      console.log("Not a match!");
     }
   })
 })
